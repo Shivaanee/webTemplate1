8ea802b420f2c39753680f0936cc04384a7858a6
Shivaanee <89640504+Shivaanee@users.noreply.github.com>
2022-03-10 20:05:34 +0400

    Contact form functionality added

--- contact/index.php
+++ contact/index.php
@@ -1,23 +1,50 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Adventure Website Design</title>
-        <link rel="stylesheet" href="style.css">
-        <script src="https://use.fontawesome.com/b9f0ac4ecc.js"></script>
-    </head>
-    <body>
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/config.php';
+
+session_start();
+
+if (!empty($_SESSION['_contact_form_error'])) {
+    $error = $_SESSION['_contact_form_error'];
+    unset($_SESSION['_contact_form_error']);
+}
+
+if (!empty($_SESSION['_contact_form_success'])) {
+    $success = true;
+    unset($_SESSION['_contact_form_success']);
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+
+    <title>Adventure Website Design</title>
+    <link rel="stylesheet" href="../style.css">
+    <script src="https://use.fontawesome.com/b9f0ac4ecc.js"></script>
+
+    <!-- reCAPTCHA Javascript -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+<body>
         <section class="sub-header">
             <nav>
-                <a href="index.html"><img src="images/Adventure-logo.png" alt="logo"></a>
+                <a href="../index.html"><img src="../images/Adventure-logo.png" alt="logo"></a>
                 <div class="nav-links" id="navLinks">
                     <i class="fa fa-times" onclick="hideMenu()"></i>
                     <ul>
-                        <li><a href="index.html">HOME</a></li>
-                        <li><a href="about.html">ABOUT</a></li>
-                        <li><a href="programs.html">PROGRAMS</a></li>
-                        <li><a href="blog.html">BLOG</a></li>
-                        <li><a href="contact.html">CONTACT</a></li>
+                        <li><a href="../index.html">HOME</a></li>
+                        <li><a href="../about.html">ABOUT</a></li>
+                        <li><a href="../programs.html">PROGRAMS</a></li>
+                        <li><a href="../blog.html">BLOG</a></li>
+                        <li><a href="index.php">CONTACT</a></li>
                     </ul>
                 </div>
                 <i class="fa fa-bars" onclick="showMenu()"></i>
@@ -57,12 +84,28 @@
                     </div>
                 </div>
                 <div class="contact-col">
-                    
-                    <form action="form-handler.php" method="post">
+                <?php
+                if (!empty($success)) {
+                    ?>
+                    <div class="alert alert-success">Your message was sent successfully!</div>
+                    <?php
+                }
+                ?>
+
+                <?php
+                if (!empty($error)) {
+                    ?>
+                    <div class="alert alert-danger"><?= $error ?></div>
+                    <?php
+                }
+                ?>
+                    <form method="post" action="submit.php">
                         <input type="text" name="name" placeholder="Name" required>
                         <input type="email" name="email" placeholder="Email" required>
                         <input type="text" name="subject" placeholder="Enter Subject" required>
                         <textarea rows="8" name="message" placeholder="Enter your Message" required></textarea>
+                        <br> <br>
+                        <div class="g-recaptcha" data-sitekey="<?= CONTACTFORM_RECAPTCHA_SITE_KEY ?>"></div>
                         <button type="submit" class="hero-btn red-btn">SUBMIT</button>
                     </form>
 
@@ -75,10 +118,10 @@
             <h4>About Us</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam vitae et enim, nostrum, quae ut impedit omnis harum eveniet <br> corrupti rerum aspernatur quaerat laborum est deserunt magnam? Consequatur, animi!</p>
             <div class="icons">
-                <i class="fa fa-facebook"></i>
-                <i class="fa fa-twitter"></i>
-                <i class="fa fa-instagram"></i>
-                <i class="fa fa-linkedin"></i>
+                <a href="https://www.facebook.com"><i class="fa fa-facebook""></i></a>
+                <a href="https://twitter.com"><i class="fa fa-twitter""></i></a>
+                <a href="https://www.instagram.com"><i class="fa fa-instagram""></i></a>
+                <a href="https://www.linkedin.com/in/shivaanee-eswaran/"><i class="fa fa-linkedin""></i></a>
             </div>
             <p>Made By <a href="https://www.linkedin.com/in/shivaanee-eswaran/">Nisha</a></p>
         </section>
@@ -93,5 +136,10 @@
                 navLinks.style.right = "-200px";
             }
         </script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
+    <!--<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>-->
+    <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>-->
     </body>
 </html>
